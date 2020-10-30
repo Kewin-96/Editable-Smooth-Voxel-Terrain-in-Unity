34d8f00bcb0f3b18b31661a47f0a6581fd9a0e98
Kewin <samwdomu1@gmail.com>
2020-10-30 18:35:41 +0100

    Initial commit

---
+++ Assets/aa.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aa : MonoBehaviour
+{
+    public MeshFilter meshRenderer;
+    public MeshCollider meshCollider;
+    public Mesh a, b;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            meshRenderer.mesh = a;
+            meshCollider.sharedMesh = a;
+        }
+        if (Input.GetKey(KeyCode.Z))
+        {
+            meshRenderer.mesh = b;
+            meshCollider.sharedMesh = b;
+        }
+    }
+}
